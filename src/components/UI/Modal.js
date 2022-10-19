48c226e1cb31cd15d9e1b00d91b1bcdc77c583a9
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-10-19 20:32:16 +0200

    Teleported the overlay up the DOM

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,35 +1,56 @@
 import React from "react";
 import Card from "./Card";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return (
+    <div
+      className="bg-slate-700 bg-opacity-50 w-full h-full z-[10] fixed"
+      onClick={props.onConfirm}
+    />
+  );
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className=" flex flex-col h-[14rem] w-[36rem] bg-white z-[100] top-60 fixed ">
+      <header className="h-[4rem] w-full bg-slate-800">
+        <h1 className="text-white font-mono font-bold text-2xl m-3">
+          {props.title}
+        </h1>
+      </header>
+      <div className="flex justify-between items-center ">
+        <p className="text-md font-mono text-lg m-auto mt-5">{props.message}</p>
+      </div>
+      <footer className="flex justify-end mt-10">
+        <button
+          className=" transition ease-in-out  mr-3 bg-slate-600 h-10 w-1/4 p-2 mt-2 text-white rounded-lg hover:bg-slate-800 hover:scale-105 duration-200 font-semibold font-mono text-lg"
+          type="submit"
+          onClick={props.onConfirm}
+        >
+          Close
+        </button>
+      </footer>
+    </Card>
+  );
+};
 
 const Modal = (props) => {
   return (
-    <>
-      <div
-        className="bg-slate-700 bg-opacity-50 w-full h-full z-[10] fixed"
-        onClick={props.onConfirm}
-      />
-      <Card className=" flex flex-col h-[14rem] w-[36rem] bg-white z-[100] top-60 fixed ">
-        <header className="h-[4rem] w-full bg-slate-800">
-          <h1 className="text-white font-mono font-bold text-2xl m-3">
-            {props.title}
-          </h1>
-        </header>
-        <div className="flex justify-between items-center ">
-          <p className="text-md font-mono text-lg m-auto mt-5">
-            {props.message}
-          </p>
-        </div>
-        <footer className="flex justify-end mt-10">
-          <button
-            className=" transition ease-in-out  mr-3 bg-slate-600 h-10 w-1/4 p-2 mt-2 text-white rounded-lg hover:bg-slate-800 hover:scale-105 duration-200 font-semibold font-mono text-lg"
-            type="submit"
-            onClick={props.onConfirm}
-          >
-            Close
-          </button>
-        </footer>
-      </Card>
-    </>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay")
+      )}
+    </React.Fragment>
   );
 };
 
