268bbc57b56d46c8bd128404e949f1d5264ba138
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-10-27 18:25:33 +0200

    Fixed the issue with logging in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,96 +1,38 @@
-// import { is } from "immer/dist/internal";
-import React, { useReducer, useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import AuthContext from "../../store/auth-context";
 import Card from "../UI/Card";
 import Modal from "../UI/Modal";
 
-const emailReducer = (action, state) => {
-  if (action.type === "EMAIL_INPUT") {
-    return {
-      value: action.val,
-      isValid: action.val.includes("@"),
-    };
-  }
-  if (action.type === "INPUT_BLUR") {
-    return {
-      value: state.value,
-      isValid: state.value.includes("@"),
-    };
-  }
-  return {
-    value: "",
-    isValid: false,
-  };
-};
-const passwordReducer = (action, state) => {
-  if (action.type === "EMAIL_INPUT") {
-    return {
-      value: action.val,
-      isValid: action.val.trim().length > 6,
-    };
-  }
-  if (action.type === "INPUT_BLUR") {
-    return {
-      value: state.value,
-      isValid: state.value.trim().length > 6,
-    };
-  }
-  return {
-    value: "",
-    isValid: false,
-  };
-};
-
 const Login = () => {
   const ctx = useContext(AuthContext);
+  const [isValid, setIsValid] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const loginHandler = ctx.loginHandler;
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: false,
-  });
-  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
-    value: "",
-    isValid: false,
-  });
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const [error, setError] = useState();
   const formSubmitHandler = (event) => {
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
     event.preventDefault();
-    if (emailState.isValid && passwordState.isValid) {
-      setIsLoggedIn(true);
-      ctx.loginHandler(isLoggedIn);
-      console.log(emailState);
-      console.log(passwordState);
+
+    if (enteredEmail.includes("@") && enteredPassword.trim().length > 6) {
+      setIsValid(true);
+      setError(null);
     }
 
-    if (!emailState.isValid || !passwordState.isValid) {
+    if (isValid) {
+      setIsLoggedIn(true);
+      ctx.loginHandler(isLoggedIn);
+    }
+
+    if (!isValid) {
       setError({
         title: "Invalid Input",
         message: "Please enter valid email or password",
       });
-      console.log(emailState);
-      console.log(passwordState);
       return;
     }
-  };
-
-  const emailChangeHandler = (event) => {
-    dispatchEmail({ type: "EMAIL_INPUT", val: event.target.value });
-    // setIsFormValid(emailState.isValid && passwordState.isValid);
-  };
-
-  const validateEmailHandler = () => {
-    dispatchEmail({ type: "INPUT_BLUR" });
-  };
-
-  const passwordChangeHandler = (event) => {
-    dispatchPassword({ type: "PASSWORD_INPUT", val: event.target.value });
-    // setIsFormValid(emailState.isValid && passwordState.isValid);
-  };
-
-  const validatePasswordHandler = () => {
-    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const confirmHandler = () => {
@@ -113,12 +55,10 @@ const Login = () => {
                 Enter Email:
               </label>
               <input
-                // ref={}
                 type="email"
                 name="user"
                 className={`w-1/2 mr-3 border-[3px] rounded-md focus:border-2 focus:outline-none  focus:border-blue-500 text-gray-500  border-slate-400 h-10 p-3 `}
-                onChange={emailChangeHandler}
-                onBlur={validateEmailHandler}
+                ref={emailInputRef}
               />
             </div>
             <div className="flex flex-col m-3">
@@ -126,19 +66,16 @@ const Login = () => {
                 Enter Password:
               </label>
               <input
-                // ref={dateInputRef}
                 type="password"
                 name="Password"
                 className={`w-1/2 mr-3 border-[3px] rounded-md focus:border-2 focus:outline-none  focus:border-blue-500 text-gray-500  border-slate-400 h-10 p-3`}
-                onChange={passwordChangeHandler}
-                onBlur={validatePasswordHandler}
+                ref={passwordInputRef}
               />
             </div>
             <div className="flex justify-end">
               <button
                 className=" transition ease-in-out  mr-3 bg-blue-400 h-10 w-1/3 p-2 mt-2 text-white rounded-lg hover:bg-indigo-500 hover:scale-105 duration-200 font-semibold font-mono text-lg"
                 type="submit"
-                // disabled={!formIsValid}
               >
                 Login
               </button>
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -4,6 +4,9 @@ import AuthContext from "../../store/auth-context";
 
 const Header = () => {
   const ctx = useContext(AuthContext);
+  const logoutHandler = (state) => {
+    ctx.logoutHandler(false);
+  };
 
   return (
     <div className="w-full bg-slate-800 h-20 mb-5 flex justify-evenly items-center pl-3">
@@ -13,7 +16,7 @@ const Header = () => {
       <button
         className=" transition ease-in-out  mr-3 bg-red-500 h-10 w-1/12 p-2 mt-1 text-white rounded-lg hover:bg-red-600 hover:scale-105 duration-200 font-semibold font-mono text-md"
         type="submit"
-        onClick={ctx.logoutHandler}
+        onClick={logoutHandler}
       >
         Logout
       </button>
